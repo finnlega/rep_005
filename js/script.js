735a4b12c268911ccf54851b9ea4f87b0734b52a
Alex <finnlega@gmail.com>
2022-03-03 10:16:31 +0800

    Начало

---
+++ js/script.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// let numbers = [1, 4, 5, 2, 9, 5, 1];
+// let UnicumNumbers = [];
+// let lastIndex = 0;
+
+// for (let i = 0; i <= numbers.length - 1; i++) {
+//     let swap = numbers[lastIndex];
+//     console.log(swap);
+//     if (swap != numbers[i]) {
+//         UnicumNumbers.push(numbers[i]);
+//         lastIndex += numbers[i];
+//     }
+//     lastIndex = numbers[i];
+//     console.log(UnicumNumbers);
+// }
+
+// var mnogo = 5;
+// const obg = {
+//     a: 50
+// };
+
+// obg.a = 10;
+// console.log(obg);
+
+// const number = 4;
+// number = 5;
+// console.log(number);
+
+// const result = confirm("Are you ready?");
+// console.log(result);
+
+// const answer =  +prompt("what is your name", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("Как ваше имя?", "");
+// answers[1] = prompt("Как ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
